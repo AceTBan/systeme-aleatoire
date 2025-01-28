ef8e1ab5f746636f800a41edb8f1f1ab275914e7
AceTBan <esteban_david@ymail.com>
2025-01-28 19:01:25 +0100

    update

--- tirage au sort.py
+++ tirage au sort.py
@@ -7,21 +7,23 @@ def lancer_des():
     return random.randint(1, 6)
 
 def jeu():
-    choix_jeu = input("Choisissez 'pile ou face' ou 'lancer de dés' : ").strip().lower()
+    choix_jeu = input("Choisissez 'pile ou face' (p) ou 'lancer de dés' (d) : ").strip().lower()
 
-    if choix_jeu == 'pile ou face':
-        choix_utilisateur = input("Choisissez 'Pile' ou 'Face' : ").strip().lower()
+    if choix_jeu == 'p':
+        choix_utilisateur = input("Choisissez 'Pile' (p) ou 'Face' (f) : ").strip().lower()
         resultat = lancer_pile_ou_face().lower()
         print(f"Le résultat est : {resultat}")
-        if choix_utilisateur == resultat:
+        if (choix_utilisateur == 'p' and resultat == 'pile') or (choix_utilisateur == 'f' and resultat == 'face'):
             print("Vous avez gagné !")
         else:
             print("Vous avez perdu.")
-    elif choix_jeu == 'lancer de dés':
+    elif choix_jeu == 'd':
         resultat = lancer_des()
         print(f"Le résultat du lancer de dés est : {resultat}")
     else:
         print("Choix invalide. Veuillez essayer à nouveau.")
+
+    input("Appuyez sur une touche pour fermer...")
 
 if __name__ == "__main__":
     jeu()
