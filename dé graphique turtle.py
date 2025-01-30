2f431bc8dddaca3a26283fabd135d242d658b7d9
AceTBan <esteban_david@ymail.com>
2025-01-30 20:45:10 +0100

    Upgrade dé graphique turtle.py

--- dé graphique turtle.py
+++ dé graphique turtle.py
@@ -1,6 +1,4 @@
-# Importer le module turtle
 import turtle
-# Importer le module random
 import random
 
 # Créer une tortue
@@ -15,12 +13,18 @@ t.speed(0)
 
 # Définir une fonction pour dessiner un carré
 def carre(cote):
+    t.up()
+    t.goto(-cote/2, cote/2)
+    t.down()
     for i in range(4):
         t.forward(cote)
         t.right(90)
 
 # Définir une fonction pour dessiner un point
-def point(diametre):
+def point(x, y, diametre):
+    t.up()
+    t.goto(x, y - diametre / 2)
+    t.down()
     t.begin_fill()
     t.circle(diametre / 2)
     t.end_fill()
@@ -35,107 +39,51 @@ def resultat(nombre):
     t.color("red")
     # Dessiner les points selon le nombre
     if nombre == 1:
-        # Un point au centre
-        t.up()
-        t.goto(0, -50)
-        t.down()
-        point(10)
+        point(0, 0, 10)
     elif nombre == 2:
-        # Deux points en diagonale
-        t.up()
-        t.goto(-25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 25)
-        t.down()
-        point(10)
+        point(-25, 25, 10)
+        point(25, -25, 10)
     elif nombre == 3:
-        # Trois points en diagonale
-        t.up()
-        t.goto(-25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(0, 0)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 25)
-        t.down()
-        point(10)
+        point(-25, 25, 10)
+        point(0, 0, 10)
+        point(25, -25, 10)
     elif nombre == 4:
-        # Quatre points aux coins
-        t.up()
-        t.goto(-25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(-25, 25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 25)
-        t.down()
-        point(10)
+        point(-25, 25, 10)
+        point(25, 25, 10)
+        point(-25, -25, 10)
+        point(25, -25, 10)
     elif nombre == 5:
-        # Cinq points dont un au centre
-        t.up()
-        t.goto(-25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(-25, 25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(0, 0)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 25)
-        t.down()
-        point(10)
-    else:
-        # Six points en deux rangées
-        t.up()
-        t.goto(-25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(-25, 0)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(-25, 25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, -25)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 0)
-        t.down()
-        point(10)
-        t.up()
-        t.goto(25, 25)
-        t.down()
-        point(10)
+        point(-25, 25, 10)
+        point(25, 25, 10)
+        point(0, 0, 10)
+        point(-25, -25, 10)
+        point(25, -25, 10)
+    elif nombre == 6:
+        point(-25, 25, 10)
+        point(25, 25, 10)
+        point(-25, 0, 10)
+        point(25, 0, 10)
+        point(-25, -25, 10)
+        point(25, -25, 10)
 
-# Créer une boucle infinie pour lancer le dé
-while True:
-    # Générer un nombre aléatoire entre 1 et 6
+# Définir une fonction pour lancer le dé
+def lancer_de():
     nombre = random.randint(1, 6)
-    # Dessiner le résultat du dé
     resultat(nombre)
-    # Attendre une seconde
-    turtle.delay(1000)
+
+# Définir une fonction pour demander à l'utilisateur s'il veut relancer ou arrêter
+def demander_relancer():
+    answer = turtle.textinput("Lancer le dé", "Voulez-vous relancer ? (oui/non)").lower()
+    if answer in ["oui", "o", "yes", "y"]:
+        lancer_de()
+    elif answer in ["non", "n", "no"]:
+        turtle.bye()
+    else:
+        demander_relancer()
+
+# Lancer le dé une première fois et demander à l'utilisateur s'il veut relancer ou arrêter
+lancer_de()
+
+# Boucle pour maintenir la fenêtre ouverte
+while True:
+    demander_relancer()
